5d2cea1844110cfa87d0d316ad5ea43a830ec49d
BernardoElizondo <is708387@iteso.mx>
2019-10-12 13:22:49 -0500

    TandaOrganizador and TandaParticipante clicks on listbox work to open selected variables

--- Proyecto/Proyecto/DetalleTandaOrganizador.Designer.cs
+++ Proyecto/Proyecto/DetalleTandaOrganizador.Designer.cs
@@ -30,9 +30,9 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lbParticipantes = new System.Windows.Forms.ListBox();
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.labelApellido = new System.Windows.Forms.Label();
             this.btnEliminar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -54,32 +54,32 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Participantes";
             // 
-            // listBox1
+            // lbParticipantes
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(15, 108);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(199, 276);
-            this.listBox1.TabIndex = 2;
+            this.lbParticipantes.FormattingEnabled = true;
+            this.lbParticipantes.ItemHeight = 16;
+            this.lbParticipantes.Location = new System.Drawing.Point(15, 108);
+            this.lbParticipantes.Name = "lbParticipantes";
+            this.lbParticipantes.Size = new System.Drawing.Size(199, 276);
+            this.lbParticipantes.TabIndex = 2;
             // 
-            // label3
+            // labelNombre
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 30);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(58, 17);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Nombre";
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.Location = new System.Drawing.Point(15, 30);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(58, 17);
+            this.labelNombre.TabIndex = 3;
+            this.labelNombre.Text = "Nombre";
             // 
-            // label4
+            // labelApellido
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(79, 30);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(58, 17);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Apellido";
+            this.labelApellido.AutoSize = true;
+            this.labelApellido.Location = new System.Drawing.Point(79, 30);
+            this.labelApellido.Name = "labelApellido";
+            this.labelApellido.Size = new System.Drawing.Size(58, 17);
+            this.labelApellido.TabIndex = 4;
+            this.labelApellido.Text = "Apellido";
             // 
             // btnEliminar
             // 
@@ -97,9 +97,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(362, 429);
             this.Controls.Add(this.btnEliminar);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.labelApellido);
+            this.Controls.Add(this.labelNombre);
+            this.Controls.Add(this.lbParticipantes);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "DetalleTandaOrganizador";
@@ -113,9 +113,9 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lbParticipantes;
+        private System.Windows.Forms.Label labelNombre;
+        private System.Windows.Forms.Label labelApellido;
         private System.Windows.Forms.Button btnEliminar;
     }
 }
