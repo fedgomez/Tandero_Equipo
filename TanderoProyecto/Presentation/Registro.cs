4b3dee53413f89c0fd22be502474fb6d723137c2
Luis_Rod_Val <luis.rodriguez@bitcon.com.mx>
2019-10-13 15:44:44 -0500

    We moves the Designs from the old proyect/folder to the new one. The register was updated to satisfy the design patterns implemented on the poryect.

--- TanderoProyecto/Presentation/Registro.cs
+++ TanderoProyecto/Presentation/Registro.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using Domain;
 
 namespace Proyecto
 {
     public partial class Registro : Form
     {
+
+        SqlConnection con = new SqlConnection(@"Data Source=CLEOPATRA;Initial Catalog=Tandero;Integrated Security=True");
+
         public Registro()
         {
             InitializeComponent();
@@ -29,7 +34,68 @@ namespace Proyecto
 
         private void btnRegister_Click(object sender, EventArgs e)
         {
-            //TO DO: Update table of users with the right values
+            if (textName.Text != "")
+            {
+                if (textEmail.Text != "")
+                {
+                    if (textPass.Text != "")
+                    {
+                        RegisterModel register = new RegisterModel();
+                        var validRegister = register.RegisterUser(textName.Text, textEmail.Text, textPass.Text);
+                        if (validRegister == true)
+                        {
+                            MessageBox.Show("Registro exitoso");
+                            this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error");
+                            textName.Clear();
+                            textEmail.Clear();
+                            textPass.Clear();
+                            textName.Focus();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error, Inserte un Password valido");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Error, Inserte un Email Valido");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error, Inserte un nombre valido");
+            }
+            
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
